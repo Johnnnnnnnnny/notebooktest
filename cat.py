72a6b1663b3269140c2c4aa8181dfbce80651769
Johnnnnnnnnny <41139822+Johnnnnnnnnny@users.noreply.github.com>
2018-07-17 15:01:02 +0800

    完整代码 可在notebook中打开

--- cat.py
+++ cat.py
@@ -12,8 +12,15 @@ notebook2_json = json.loads(notebook2_str)
 cells2 = notebook2_json['cells']
 target_cells = cells1 + cells2
 
+del notebook1_json['cells']
+target_notebook = {}
+target_notebook['cells'] = target_cells
+target_notebook.update(notebook1_json)
 
-target_json = json.dumps(target_cells)
+
+
+
+target_json = json.dumps(target_notebook)
 
 target = open('target_notebook.ipynb','w')
 target.write(target_json)
