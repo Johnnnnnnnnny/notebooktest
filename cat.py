e23537dfef7c1b8d06ac328a5559f959b2af51d0
Johnnnnnnnnny <41139822+Johnnnnnnnnny@users.noreply.github.com>
2018-07-17 12:23:32 +0800

    正确代码

--- cat.py
+++ cat.py
@@ -1,7 +1,13 @@
+import json
 
-
-notebook1 = {}
-cells1 = notebook1['cells']
-notebook2 = {}
-cells2 = notebook2['cells']
+notebook1 = open('Untitled1.ipynb')
+notebook1_str = notebook1.read()
+notebook1_json = json.loads(notebook1_str)
+print(notebook1_json)
+cells1 = notebook1_json['cells']
+notebook2 = open('Untitled2.ipynb')
+notebook2_str = notebook2.read()
+notebook2_json = json.loads(notebook2_str)
+print(notebook2_json)
+cells2 = notebook2_json['cells']
 target_cells = cells1 + cells2
